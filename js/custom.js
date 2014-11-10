a4ed01b9aebfb549b6bb138f6beed4b6b9858be2
Snake174 <jonnysereb@gmail.com>
2014-11-10 08:13:58 +0500

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -9,19 +9,5 @@ $(document).ready( function() {
   }, 'text' );
   
   $('#[id ^= table_]').dataTable();
-
-  // Вывод главной страницы
-  /*$.get( 'html/start.txt', function( data ) {
-      $('div#content').html( data );
-    }, 'text' );
-
-  // Обработка кликов по меню
-  $('body').on( 'click', '#menu a', function(e) {
-    e.preventDefault();
-
-    $.get( $(this).attr('data'), function( data ) {
-      $('div#content').html( data );
-    }, 'text' );
-  })*/
-
+  
 } );
