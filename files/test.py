5f7c944d1d5ae885b8a4d03c96d434bd861e965e
Snake174 <jonnysereb@gmail.com>
2015-01-20 13:19:28 +0600

    test python

--- files/test.py
+++ files/test.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python
 
-import os
+# Import modules for CGI handling 
+import cgi, cgitb 
 
-print "Content-type: text/html\r\n\r\n";
-print "<font size=+1>Environment</font><\br>";
-for param in os.environ.keys():
-  print "<b>%20s</b>: %s<\br>" % (param, os.environ[param])
+# Create instance of FieldStorage 
+form = cgi.FieldStorage() 
+
+# Get data from fields
+first_name = form.getvalue('first_name')
+last_name  = form.getvalue('last_name')
+
+print "Content-type:text/html\r\n\r\n"
+print "<html>"
+print "<head>"
+print "<title>Hello - Second CGI Program</title>"
+print "</head>"
+print "<body>"
+print "<h2>Hello %s %s</h2>" % (first_name, last_name)
+print "</body>"
+print "</html>"
