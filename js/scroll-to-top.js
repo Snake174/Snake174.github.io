9d80db4aa413bb55f5e9aeed15e535375bfeb861
Snake174 <jonnysereb@gmail.com>
2015-04-13 16:36:29 +0600

    optimize

--- js/scroll-to-top.js
+++ js/scroll-to-top.js
@@ -1,5 +1,1 @@
-$(document).ready( function() {
-  $(window).scroll( function() {
-    $(this).scrollTop() >= 100 ? $('#top-link-block').show() : $('#top-link-block').hide();
-  } );
-} );
+$(document).ready(function(){$(window).scroll(function(){$(this).scrollTop()>=100?$('#top-link-block').show():$('#top-link-block').hide();});});
