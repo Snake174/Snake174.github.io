a636b369240ff87da76dc9b6c05d55748a1422c4
Snake174 <jonnysereb@gmail.com>
2015-04-13 10:31:18 +0600

    restyle

--- js/sendMail.js
+++ js/sendMail.js
@@ -8,7 +8,83 @@ $(document).ready( function() {
     }
   );
 
-  function checkEmail( emailAddress )
+  //****************************************************************
+  $('#contact_form').bootstrapValidator( {
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+    fields: {
+      first_name: {
+        validators: {
+          stringLength: {
+            min: 2,
+          },
+          notEmpty: {
+            message: 'Введите имя'
+          }
+        }
+      },
+      email: {
+        validators: {
+          notEmpty: {
+            message: 'Введите E-Mail'
+          },
+          emailAddress: {
+            message: 'Введённый E-Mail некорректен'
+          }
+        }
+      },
+      comment: {
+        validators: {
+          stringLength: {
+            min: 10,
+            message: 'Длина сообщения должна быть более 10 символов'
+          },
+          notEmpty: {
+            message: 'Введите сообщение'
+          }
+        }
+      }
+    }
+  } )
+  .on( 'success.form.bv', function(e) {
+    e.preventDefault();
+
+    var name = $('input[name=first_name]').val();
+    var email = $('input[name=email]').val();
+    var body = $('#contact_form form #body').val();
+
+    $.ajax( {
+      type: 'POST',
+      url: 'https://mandrillapp.com/api/1.0/messages/send.json',
+      data: {
+        'key': 'atuMPWDK1OaYmmxeH_28Lg',
+        'message': {
+          'from_email': email,
+          'to': [
+            {
+              'email': 'burlachenkomaxim@gmail.com',
+              'type': 'to'
+            },
+            {
+              'email': 'jonnysereb@gmail.com',
+              'type': 'to'
+            }
+          ],
+          'autotext': 'true',
+          'subject': 'Статьи, игры, программы - Обратная связь',
+          'html': 'Письмо от <b>' + name + '</b><br/><br/>' + body + '<br/><br/><a href="http://snake174.github.io/" target="_blank">Статьи, игры, программы</a>'
+        }
+      }
+    } ).done( function( response ) {
+      $('#contact-form-wrap').html('<font color=#00AA00>Письмо успешно отправлено. Спасибо за посещение нашего ресурса.</font>');
+    } );
+  } );
+  //****************************************************************
+
+  /*function checkEmail( emailAddress )
   {
     var sQtext = '[^\\x0d\\x22\\x5c\\x80-\\xff]';
     var sDtext = '[^\\x0d\\x5b-\\x5d\\x80-\\xff]';
@@ -31,19 +107,19 @@ $(document).ready( function() {
 
     return false;
   }
-  
+
   $('#contact_form form').live( 'submit', function(e) {
       e.preventDefault();
 
       var email = $('input[name=email]').val();
       var body = $('#contact_form form #body').val();
-      
+
       if (email == '' || body == '')
       {
         $('.contacts_status').html('<b><font color=#AA0000>Введите данные</font></b>');
         return;
       }
-      
+
       if (checkEmail( email ) == false)
       {
         $('.contacts_status').html('<b><font color=#AA0000>Некорректный E-mail</font></b>');
@@ -82,6 +158,6 @@ $(document).ready( function() {
         $('#contact_form').html('<font color=#00AA00>Письмо успешно отправлено. Спасибо за посещение нашего ресурса.</font>');
       });
     }
-  )
+  )*/
 
 } );
