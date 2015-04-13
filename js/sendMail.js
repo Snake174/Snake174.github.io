2acb27dccf6d7f7bf17485c75cc09098879fe711
Snake174 <jonnysereb@gmail.com>
2015-04-13 11:52:19 +0600

    restyle

--- js/sendMail.js
+++ js/sendMail.js
@@ -87,7 +87,7 @@ $(document).ready( function() {
       }
     } ).done( function( response ) {
       $('#contact-form-wrap').html('<font color=#00AA00>Письмо успешно отправлено. Спасибо за посещение нашего ресурса.</font>');
-    } ).fail( function( jqXHR, textStatusresponse, q, t ) {
+    } ).fail( function( response, q, t ) {
       var r = jQuery.parseJSON( response.responseText );
       $('#contact-form-wrap').html('<font color=#AA0000><b>Ошибка:</b></font> ' + r.Message);
     } );
