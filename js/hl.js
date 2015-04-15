34c5e799e9ae6e3923169f94967a4a5e45f963b3
Snake174 <jonnysereb@gmail.com>
2015-04-15 17:46:43 +0600

    restyle

--- js/hl.js
+++ js/hl.js
@@ -1,11 +1,11 @@
 $(document).ready( function() {
   hljs.configure( {
     tabReplace: '  ',
-    classPrefix: '',
-    useBR: true
+    classPrefix: ''
   } );
-  $('pre code').each( function( i, block ) {
+  /*$('pre code').each( function( i, block ) {
     hljs.highlightBlock( block );
-  } );
+  } );*/
+  hljs.initHighlighting();
 } );
 
