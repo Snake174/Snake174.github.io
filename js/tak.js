5f716264e900eb86771022af75f4613e57ce9aad
Snake174 <jonnysereb@gmail.com>
2015-04-28 16:40:05 +0600

    tak

--- js/tak.js
+++ js/tak.js
@@ -2,6 +2,7 @@ $(document).ready( function() {
   $('a.takru').each( function( i, e ) {
     if ($(e).href == 'http://www.tak.ru')
     {
+      alert( $(e).href );
       $(e).attr( 'href', 'http://tak.ru/partner.php?id=934977' );
     }
   } );
