95087fc507d0e7adc23d2c820872d7dbb75fb92c
Snake174 <jonnysereb@gmail.com>
2015-04-30 12:34:05 +0600

    bg

--- js/bg.js
+++ js/bg.js
@@ -1,14 +1,18 @@
-var currentPageUrlIs = '';
-
-if (typeof this.href != 'undefined')
-  currentPageUrlIs = this.href.toString().toLowerCase(); 
-else
-  currentPageUrlIs = document.location.toString().toLowerCase();
-
 $(document).ready( function() {
+
+  var currentPageUrlIs = '';
+
+  if (typeof this.href != 'undefined')
+    currentPageUrlIs = this.href.toString().toLowerCase();
+  else
+    currentPageUrlIs = document.location.toString().toLowerCase();
+
+  console.log( currentPageUrlIs );
+
   if (currentPageUrlIs == 'http://snake174.github.io/html/games/birds.html?p=1')
   {
     console.log('Show programmly');
   }
+
 } )
 
