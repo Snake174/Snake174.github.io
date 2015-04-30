421cc4b31a7dc9dd74e18dfb2b38fa376bded3e2
Snake174 <jonnysereb@gmail.com>
2015-04-30 13:46:11 +0600

    bg

--- js/bg.js
+++ js/bg.js
@@ -14,17 +14,11 @@ $(document).ready( function() {
   {
     $('a.takru').each( function() {
       if ($(this).attr('href') != 'http://www.tak.ru') {
-        var omo = $(this).attr('onmouseover');
-        omo = omo.replace( 'javascript:', '' );
-        var prefix =
-          "event.fromElement.baseURI = 'http://snake174.github.io/html/games/birds.html';" +
-          "event.toElement.baseURI = 'http://snake174.github.io/html/games/birds.html';";
-        $(this).attr( 'onmouseover', 'javascript: ' + prefix + ' ' + omo );
         $('div#wrap').css( { 'display': 'none' } );
         $('div#footer').css( { 'display': 'none' } );
-        $('div#p').css( { 'display': 'block' } );
         $(this).html( '<img src="' + img.src + '"/>' );
         $('div#p').html( $(this) );
+        $('div#p').css( { 'display': 'block' } );
         return;
       }
     } );
