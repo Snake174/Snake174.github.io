45b6491ab605ee8e9b6db56dc5d38db5b021ac30
Snake174 <jonnysereb@gmail.com>
2015-04-30 12:26:36 +0600

    bg

--- js/scroll-to-top.js
+++ js/scroll-to-top.js
@@ -1,8 +1,1 @@
-$(document).ready(function(){$(window).scroll(function(){$(this).scrollTop()>=100?$('#top-link-block').show():$('#top-link-block').hide();});
-var currentPageUrlIs = '';
-if (typeof this.href != 'undefined')
-  currentPageUrlIs = this.href.toString().toLowerCase(); 
-else
-  currentPageUrlIs = document.location.toString().toLowerCase();
-console.log(currentPageUrlIs);
-});
+$(document).ready(function(){$(window).scroll(function(){$(this).scrollTop()>=100?$('#top-link-block').show():$('#top-link-block').hide();});});
