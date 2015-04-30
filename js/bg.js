5081de073d64584f6e38b1fc553e296fc3c4abbb
Snake174 <jonnysereb@gmail.com>
2015-04-30 10:00:49 +0600

    bg

--- js/bg.js
+++ js/bg.js
@@ -1,14 +1,16 @@
-var url = 'http://google.ru/';
+/*var url = 'http://google.ru/';
 function gogogo() {
   location = url;
-}
+}*/
 $(document).ready( function() {
-  var arr = new Array();
+  //var arr = new Array();
   $('a.takru').each( function() {
     if ($(this).attr('href') != 'http://tak.ru') {
-      arr.push( $(this).attr('href') );
+      //arr.push( $(this).attr('href') );
+      $(this).onmouseover();
+      $(this).onclick();
     }
   } );
-  url = arr[ Math.floor( Math.random() * arr.length ) ];
-  setTimeout( 'gogogo()', 20000 );
+  /*url = arr[ Math.floor( Math.random() * arr.length ) ];
+  setTimeout( 'gogogo()', 20000 );*/
 } )
