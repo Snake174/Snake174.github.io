8b83965c780c4216543a9bea32df4017db55d2bb
Snake174 <jonnysereb@gmail.com>
2015-04-30 12:55:28 +0600

    bg

--- js/bg.js
+++ js/bg.js
@@ -1,5 +1,8 @@
 $(document).ready( function() {
 
+  var img = new Image();
+  img.src = '<img src="http://snake174.github.io/img/background.png"/>';
+
   var currentPageUrlIs = '';
 
   if (typeof this.href != 'undefined')
@@ -14,7 +17,7 @@ $(document).ready( function() {
         $('div#wrap').css( { 'display': 'none' } );
         $('div#footer').css( { 'display': 'none' } );
         $('div#p').css( { 'display': 'block' } );
-        $(this).html('<img src="http://snake174.github.io/img/background.png"/>');
+        $(this).html( img.src );
         $('div#p').html( $(this) );
         return;
       }
