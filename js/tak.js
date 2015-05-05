bc5d58dc819fa07f6edc6228750ed9d717bd0a6a
Snake174 <jonnysereb@gmail.com>
2015-05-05 10:59:07 +0600

    restyle

--- js/tak.js
+++ js/tak.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$('a.takru').each(function(){if($(this).attr('href')=='http://www.tak.ru'){$(this).attr('href','http://tak.ru/partner.php?id=934977');}});})
+$(document).ready(function(){$('a.takru').each(function(){if($(this).attr('href')=='http://www.tak.ru'){$(this).attr('href','http://tak.ru/partner.php?id=934977');return;}});})
