29e6a65f364a23e88c276709ec9dfec724202f89
Snake174 <jonnysereb@gmail.com>
2015-05-27 01:50:06 +0600

    race
    race

--- html/other/RACE/js/intro.js
+++ html/other/RACE/js/intro.js
@@ -2,17 +2,22 @@ Intro = Backbone.View.extend( {
   initialize: function() {
     var view = this;
     this.doc = document;
-    this.canvas = this.doc.getElementById('game-canvas');
-    this.ctx = this.canvas.getContext('2d');
+    //this.canvas = this.doc.getElementById('game-canvas');
+    //this.ctx = this.canvas.getContext('2d');
+    this.canvas = this.$el.get(0); 
+    this.ctx = this.$el.get(0).getContext('2d');
     this.doc.getElementById('loading').style.display = 'none';
     this.logo = new Image();
+    this.logo.onload = function() { view.onResize(); view.render(); };
     this.logo.src = '../img/logo.png';
 	  $(window).on( 'resize', function() { view.onResize(); } );
-    view.onResize();
-    view.render();
   },
   render: function() {
     this.ctx.drawImage( this.logo, 100, 100 );
+    this.ctx.fillStyle = "#FF9000";
+    this.ctx.globalAlpha = 1;
+    this.ctx.fillRect(10, 10, 100, 100);
+    console.log('000');
   },
   onResize: function() {
     var rc = this.doc.getElementById('game-container').getBoundingClientRect();
