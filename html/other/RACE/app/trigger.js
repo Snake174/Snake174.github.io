c8e763f247325040d24da53047da76e8f9670500
Snake174 <jonnysereb@gmail.com>
2015-07-15 01:50:06 +0600

    race

--- html/other/RACE/app/trigger.js
+++ html/other/RACE/app/trigger.js
@@ -28,7 +28,7 @@ TrackTriggers.prototype.update = function( car ) {
 
     if (len <= 45) {
       if (!t.isTriggered) {
-        if (this.currentLap == 0 && i == 0 && this.triggers[2].isTriggered == false && this.triggers[2].isTriggered == false)
+        if (this.currentLap == 0 && i == 0 && this.triggers[1].isTriggered == false && this.triggers[2].isTriggered == false)
           return;
 
         console.log( this.currentLap );
