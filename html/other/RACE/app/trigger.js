9f4f5aab91632bd3efc8b5f2ab7fb89f319aef01
Snake174 <jonnysereb@gmail.com>
2015-07-15 02:00:06 +0600

    race

--- html/other/RACE/app/trigger.js
+++ html/other/RACE/app/trigger.js
@@ -13,6 +13,9 @@ var TrackTriggers = function() {
   this.triggers = new Array();
   this.currentLap = 0;
   this.done = false;
+  this.curTrigger = 1;
+  this.needTriggers = 9;
+  this.trigger = 0;
 }
 
 TrackTriggers.prototype.add = function( trigger ) {
@@ -20,48 +23,27 @@ TrackTriggers.prototype.add = function( trigger ) {
 }
 
 TrackTriggers.prototype.update = function( car ) {
-  for (var i = 0; i < 3; ++i) {
-    var t = this.triggers[i];
-    var dx = t.pos.x - car.pos.x;
-    var dy = t.pos.y - car.pos.y;
-    var len = Math.sqrt( dx * dx + dy * dy );
+  var t = this.triggers[ this.curTrigger ];
+  var dx = t.pos.x - car.pos.x;
+  var dy = t.pos.y - car.pos.y;
+  var len = Math.sqrt( dx * dx + dy * dy );
 
-    if (len <= 45) {
-      if (!t.isTriggered) {
-        if (this.currentLap == 0 && i == 0 && this.triggers[1].isTriggered == false && this.triggers[2].isTriggered == false)
-          return;
+  if (len <= 45) {
+    if (!t.isTriggered) {
+      t.isTriggered = true;
 
-        console.log( this.currentLap );
+      ++this.trigger;
 
-        t.isTriggered = true;
+      if (this.triggers == this.needTriggers)
+        this.done = true;
 
-        if (this.triggers[0].isTriggered == true
-          && this.triggers[1].isTriggered == true
-          && this.triggers[2].isTriggered == true) {
+      if (this.trigger % 3 == 0)
+        ++this.currentLap;
 
-          ++this.currentLap;
-
-          if (this.currentLap == 3)
-            this.done = true;
-
-          for (var i = 0; i < 3; ++i)
-            this.triggers[i].isTriggered = false;
-        }
-      }
+      console.log( this.currentLap );
+      console.log( this.trigger );
     }
   }
-}
-
-TrackTriggers.prototype.isLapComplete = function() {
-  return
-    this.triggers[0].isTriggered == true
-    && this.triggers[1].isTriggered == true
-    && this.triggers[2].isTriggered == true;
-}
-
-TrackTriggers.prototype.clear = function() {
-  for (var i = 0; i < 3; ++i)
-    this.triggers[i].isTriggered = false;
 }
 
 var trackTriggers = new TrackTriggers();
