e5e98420174decaba699cfb8333b22863500dc7d
Snake174 <jonnysereb@gmail.com>
2015-07-15 00:35:28 +0600

    race

--- html/other/RACE/app/trigger.js
+++ html/other/RACE/app/trigger.js
@@ -1,5 +1,6 @@
 var Trigger = function( x, y ) {
   this.pos = new Vec2( x, y );
+  this.isTriggered = false;
 }
 
 var triggersPos = new Array(
@@ -19,8 +20,9 @@ TrackTriggers.prototype.add = function( trigger ) {
 
 TrackTriggers.prototype.update = function( car ) {
   for (var i = 0; i < this.triggers.length; ++i) {
-    var dx = this.triggers[i].pos.x - car.pos.x;
-    var dy = this.triggers[i].pos.y - car.pos.y;
+    var t = this.triggers[i];
+    var dx = t.pos.x - car.pos.x;
+    var dy = t.pos.y - car.pos.y;
     var len = Math.sqrt( dx * dx + dy * dy );
 
     if (len <= 50) {
